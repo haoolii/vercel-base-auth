a160c44d9db26487195e58d7f0a44ce0044f183a
haoolii <unnhao@gmail.com>
2025-02-04 17:55:20 +0800

    fix: build error

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 
-export async function GET(request: Request) {
+export async function GET() {
     const response = NextResponse.json({ message: "Auth Required." }, { status: 401 });
     response.headers.set("WWW-authenticate", 'Basic realm="Secure Area"');
     return response;
